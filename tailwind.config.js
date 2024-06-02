ee6abf6c5aacc45382fc47ad91dc36eeff30b711
李大毛 <github@darmau.co>
2024-06-02 12:33:04 +0900

    setup tailwind forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms';
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    forms
+  ],
 }
 
