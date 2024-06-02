7e3d0b5f2ec7c8885ba75e7d69a6a59669240f76
李大毛 <github@darmau.co>
2024-06-02 13:34:38 +0900

    feat: basic language methods

---
+++ src/lib/components/setting/AddLanguage.svelte
@@ -0,0 +1,39 @@
+<!--本组件是一个toast，用于添加语言-->
+<script lang="ts">
+	import InputField from '$components/InputField.svelte';
+
+	export let closeAddLanguage: () => void;
+	export let addLanguage: (lang: string, locale: string) => void;
+
+	function submitForm(event: Event) {
+		event.preventDefault();
+		const formData = new FormData(event.target as HTMLFormElement);
+		const languageData = Object.fromEntries(formData.entries());
+		addLanguage(languageData.lang, languageData.locale);
+		closeAddLanguage();
+	}
+</script>
+
+<form
+	method="POST"
+	on:submit={submitForm}
+>
+  <InputField
+		label="Language"
+		name="lang"
+		type="text"
+		placeholder="Input language"
+	/>
+  <InputField
+		label="Locale"
+		name="locale"
+		type="text"
+		placeholder="Input locale"
+	/>
+	<button
+		type="submit"
+		class="bg-cyan-600 text-white rounded px-4 py-2 hover:bg-cyan-800"
+	>
+		Submit
+	</button>
+</form>
--- src/lib/components/setting/Language.svelte
+++ src/lib/components/setting/Language.svelte
@@ -2,44 +2,68 @@
 	import PlusIcon from '$assets/icons/plus.svelte';
 	import { t } from '$lib/functions/i18n'
 	import { onMount } from 'svelte';
+	import AddLanguage from '$components/setting/AddLanguage.svelte';
 
-	export let data
+	export let data;
+	let errorMessage = '';
 	let languages = [];
 	let { supabase } = data
 	$: ({ supabase } = data)
 
 	// 获取所有语言
-	$: getLanguages = async () => {
-		const { data } = await supabase.from('language').select('lang, locale, is_default');
+	const getLanguages = async () => {
+		const { data } = await
+			supabase.from('language').select('lang, locale, is_default').order('is_default', {ascending: false});
 		languages = data;
-	}
-
-	$: addLanguage = async () => {
-		await supabase.from('language').insert({lang: 'jp',
-			locale: '日本语'}).select();
-		await getLanguages();
-	}
-
-	// 更换默认语言
-	$: setDefaultLanguage = async (lang: string) => {
-		await supabase.from('language').update({is_default: true}).eq('lang', lang);
-		await getLanguages();
 	}
 
 	onMount(async () => {
 		await getLanguages();
 	});
 
-	$: addTestLanguage = async () => {
-		const { data } = await supabase.from('language').insert({lang: 'jp',
-			locale: '日本语'}).select();
-		return data;
+	// 更换默认语言
+	const setDefaultLanguage = async (lang: string) => {
+		await supabase.from('language').update({is_default: true}).eq('lang', lang);
+		await getLanguages();
 	}
+
+	// 添加语言
+	const addLanguage = async (lang, locale) => {
+		const { data, error: dataError } = await
+			supabase.from('language').insert({ lang, locale }).select();
+		if (dataError) {
+			errorMessage = dataError.message;
+		}
+		closeAddLanguage();
+	}
+
+	// 删除语言
+	const deleteLanguage = async (lang: string) => {
+		const {error: deleteError} = await
+			supabase.from('language').delete().eq('lang',
+			lang);
+		if (deleteError) {
+			errorMessage = deleteError.message;
+			console.error(deleteError);
+		}
+		await getLanguages();
+	}
+
+	// 关闭添加语言窗口并刷新数据
+	let isAdding: boolean = false;
+	function closeAddLanguage() {
+		isAdding = false;
+		getLanguages();
+	}
+
+	// 错误信息alert
+	let errorVisible: boolean = true;
+
 </script>
 
 <main class="py-8 flex flex-col gap-4">
 	<button
-		on:click = {addLanguage}
+		on:click = {() => isAdding = true}
 		type = "button"
 		class =
 			"self-end inline-flex items-center gap-x-1.5 rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -47,6 +71,12 @@
 		<PlusIcon classList="w-4 h-4" />
 		{$t('add-language')}
 	</button>
+	{#if isAdding}
+		<AddLanguage {closeAddLanguage} {addLanguage} />
+	{/if}
+	{#if errorMessage}
+		<p>{errorMessage}</p>
+	{/if}
 	<div>
 			{#each languages as language}
 				<div class="border-b flex justify-between py-4">
@@ -71,6 +101,7 @@
 						{/if}
 						<button
 							type = "button"
+							on:click = {() => deleteLanguage(language.lang)}
 							class="uppercase text-sm text-red-600 hover:text-red-700 hover:font-semibold"
 						>{$t('delete')}
 						</button>
