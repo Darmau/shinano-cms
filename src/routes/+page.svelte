d4475820a3f8f46233b398b85b4033aec0a2500e
李大毛 <github@darmau.co>
2024-06-12 20:34:29 +0900

    style: 优化默认首页样式

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,17 @@
-<main>
-	<a href="/admin">进入首页</a>
+<main class="bg-white h-screen flex items-center justify-center">
+	<div class="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+		<div class="mx-auto max-w-2xl text-center">
+			<h2
+				class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+				信濃CMS</h2>
+			<p
+				class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">可能是最不通用的CMS
+			</p>
+			<div class="mt-10 flex items-center justify-center gap-x-6">
+				<a href="/admin"
+					 class="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">Dashboard</a>
+				<a href="https://github.com/Darmau/shinano-cms" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+			</div>
+		</div>
+	</div>
 </main>
