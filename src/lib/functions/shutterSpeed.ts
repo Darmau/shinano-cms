4142ce4f805271b4e4c26f7269069355e760031b
李大毛 <github@darmau.co>
2024-06-14 23:05:10 +0900

    fix: 快门时间可以为undefined

--- src/lib/functions/shutterSpeed.ts
+++ src/lib/functions/shutterSpeed.ts
@@ -1,6 +1,6 @@
 // 本文件接收一个float快门速度参数，返回一个1/125类型的string快门速度参数
-export default function shutterSpeed(shutterTime: string): string {
-	if (!shutterTime) return "0";
+export default function shutterSpeed(shutterTime: string): string | undefined {
+	if (!shutterTime) return undefined;
 	const time = parseFloat(shutterTime);
 	if (time >= 1) {
 		// 保留整数部分
