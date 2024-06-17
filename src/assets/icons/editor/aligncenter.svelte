5b90afc8daaf4e017f38a3a5fbda57243285c535
李大毛 <github@darmau.co>
2024-06-17 20:47:28 +0900

    style: align icons

---
+++ src/assets/icons/editor/aligncenter.svelte
@@ -0,0 +1,13 @@
+<script lang = "ts">
+	export let classList = '';
+</script>
+
+<svg
+	width = "24" height = "24" viewBox = "0 0 24 24" fill = "none" class =
+	{classList} xmlns = "http://www.w3.org/2000/svg"
+>
+	<path
+		d = "M7.5 5.5H16.5V4H7.5V5.5ZM4 12.5H20V11H4V12.5ZM7.5 19.5H16.5V18H7.5V19.5Z"
+		fill = "currentColor"
+	/>
+</svg>
---
+++ src/assets/icons/editor/alignleft.svelte
@@ -0,0 +1,13 @@
+<script lang = "ts">
+	export let classList = '';
+</script>
+
+<svg
+	width = "24" height = "24" viewBox = "0 0 24 24" fill = "none" class =
+	{classList} xmlns = "http://www.w3.org/2000/svg"
+>
+	<path
+		d = "M13 5.5H4V4H13V5.5ZM20 12.5H4V11H20V12.5ZM13 19.5H4V18H13V19.5Z"
+		fill = "currentColor"
+	/>
+</svg>
