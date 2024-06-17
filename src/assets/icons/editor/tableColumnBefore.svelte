446762f0b58e51650b2bdb2a4699b97731570255
李大毛 <github@darmau.co>
2024-06-17 20:51:53 +0900

    style: table icons

---
+++ src/assets/icons/editor/tableColumnBefore.svelte
@@ -0,0 +1,13 @@
+<script lang = "ts">
+	export let classList = '';
+</script>
+
+<svg
+	width = "24" height = "24" viewBox = "0 0 24 24" fill = "none" class =
+	{classList} xmlns = "http://www.w3.org/2000/svg"
+>
+	<path
+		d = "M8.4 5.776V9.424H4.752V11.216H8.4V14.864H10.128V11.216H13.84V9.424H10.128V5.776H8.4ZM2 19.92V2H22.48V19.92H2ZM14.8 3.28H3.28V17.36H14.8V3.28ZM21.2 3.28H16.08V7.12H21.2V3.28ZM21.2 8.4H16.08V12.24H21.2V8.4ZM21.2 13.52H16.08V17.36H21.2V13.52Z"
+		fill = "currentColor"
+	/>
+</svg>
---
+++ src/assets/icons/editor/tableRowDelete.svelte
@@ -0,0 +1,13 @@
+<script lang = "ts">
+	export let classList = '';
+</script>
+
+<svg
+	width = "24" height = "24" viewBox = "0 0 24 24" fill = "none" class =
+	{classList} xmlns = "http://www.w3.org/2000/svg"
+>
+	<path
+		d = "M19.728 13.456L16.592 10.32L19.792 7.12L18.256 5.584L15.056 8.784L11.92 5.648L10.384 7.12L13.584 10.32L10.32 13.584L11.856 15.12L15.12 11.856L18.256 14.992L19.728 13.456ZM2 19.92V2H22.48V19.92H2ZM21.2 13.52H20.752L19.472 12.24H21.2V8.4H19.408L20.688 7.12H21.2V3.28H3.28V7.12H9.488L10.768 8.4H3.28V12.24H10.704L9.424 13.52H3.28V17.36H21.2V13.52Z"
+		fill = "currentColor"
+	/>
+</svg>
