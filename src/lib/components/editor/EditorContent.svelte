3e27c244beac92bf30934f41c74e42c046301744
李大毛 <github@darmau.co>
2024-06-17 18:34:41 +0900

    feat: tiptap基础功能

---
+++ src/lib/components/editor/EditorContent.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { beforeUpdate, onDestroy, onMount, tick } from 'svelte';
+	import type { Editor } from '$lib/functions/Editor';
+
+	let element: HTMLElement;
+	export let editor: Editor;
+
+	const init = async () => {
+		await tick();
+		if (!editor?.options.element) {
+			return;
+		}
+
+		if (editor.contentElement) {
+			return;
+		}
+
+		element.append(...Array.from(editor.options.element.childNodes));
+		editor.setOptions({ element });
+
+		editor.contentElement = element;
+	};
+
+	onMount(init);
+	beforeUpdate(init);
+
+	onDestroy(() => {
+		if (!editor) {
+			return;
+		}
+
+		editor.contentElement = null;
+
+		if (!editor.options.element.firstChild) {
+			return;
+		}
+
+		const newElement = document.createElement('div');
+		newElement.append(...Array.from(editor.options.element.childNodes));
+
+		editor.setOptions({
+			element: newElement,
+		});
+	});
+</script>
+
+<div class="prose prose-zinc max-w-none" bind:this={element} />
+<slot />
--- src/lib/components/editor/Tiptap.svelte
+++ src/lib/components/editor/Tiptap.svelte
@@ -1,72 +1,239 @@
 <script lang="ts">
-	import { onMount, onDestroy } from 'svelte';
-	import { writable } from 'svelte/store';
-	import { Editor } from '@tiptap/core';
+	import { onMount } from 'svelte';
+	import type { Readable } from 'svelte/store';
 	import StarterKit from '@tiptap/starter-kit';
-	import FixedMenu from '$components/editor/FixedMenu.svelte';
+	import Highlight from '@tiptap/extension-highlight';
+	import { Link } from '@tiptap/extension-link';
+	import { Editor } from '$lib/functions/Editor';
+	import EditorContent from '$components/editor/EditorContent.svelte';
+	import createEditor from '$lib/functions/createEditor';
 
-	let element;
-	let editor;
+	// import { SvelteCounterExtension, SvelteEditableExtension } from './_components/SvelteExtension';
 
-	export let content = '';
-
-	const contentStore = writable(content);
+	let editor: Readable<Editor>;
 
 	onMount(() => {
-		editor = new Editor({
-			element: element,
+		editor = createEditor({
 			extensions: [
 				StarterKit,
+				Highlight,
+				Link.configure({
+					protocols: ['http', 'https', 'mailto'],
+					defaultProtocol: 'https',
+				})
 			],
+			content: `
+        <p>This is still the text editor you're used to, but enriched with node views.</p>
+        <svelte-counter-component count="0"></svelte-counter-component>
+        <p>This is an editable component</p>
+        <svelte-editable-component>This is editable</svelte-editable-component>
+        <p>Did you see that? That's a Svelte component. We are really living in the future.</p>
+      `,
 			editorProps: {
 				attributes: {
-					class: 'prose lg:prose-xl'
-				}
+					class: 'border-2 border-black rounded-b-md p-3 outline-none',
+				},
 			},
-			content: '<p>Hello World! 🌍️ </p>',
-			onTransaction: () => {
-				// force re-render so `editor.isActive` works as expected
-				editor = editor;
-			}
-		});
-		editor.on('update', ({ editor }) => {
-			contentStore.set(editor.getHTML());
 		});
 	});
 
-	onDestroy(() => {
-		if (editor) {
-			editor.destroy();
+	const toggleHeading = (level: 1 | 2) => {
+		return () => {
+			$editor.chain().focus().toggleHeading({ level }).run();
+		};
+	};
+
+	const toggleBold = () => {
+		$editor.chain().focus().toggleBold().run();
+	};
+
+	const toggleItalic = () => {
+		$editor.chain().focus().toggleItalic().run();
+	};
+
+	const toggleStrike = () => {
+		$editor.chain().focus().toggleStrike().run();
+	};
+
+	const toggleInlineCode = () => {
+		$editor.chain().focus().toggleCode().run();
+	};
+
+	const toggleHighlight = () => {
+		$editor.chain().focus().toggleHighlight().run();
+	};
+
+	const setLink = () => {
+		const previousUrl = $editor.getAttributes('link').href
+		const url = window.prompt('URL', previousUrl)
+
+		// cancelled
+		if (url === null) {
+			return
 		}
-	});
+
+		// empty
+		if (url === '') {
+			$editor.chain().focus().extendMarkRange('link').unsetLink()
+			.run()
+
+			return
+		}
+
+		// update link
+		$editor.chain().focus().extendMarkRange('link').setLink({ href: url })
+		.run()
+	};
+
+	const setParagraph = () => {
+		$editor.chain().focus().setParagraph().run();
+	};
+
+	const toggleBulletList = () => {
+		$editor.chain().focus().toggleBulletList().run();
+	};
+
+	const toggleOrderedList = () => {
+		$editor.chain().focus().toggleOrderedList().run();
+	};
+
+	const toggleCodeBlock = () => {
+		$editor.chain().focus().toggleCodeBlock().run();
+	};
+
+	const setHardBreak = () => {
+		$editor.chain().focus().setHardBreak().run();
+	};
+
+	const setDivider = () => {
+		$editor.chain().focus().setHorizontalRule().run();
+	};
+
+	$: isActive = (name: string, attrs = {}) => $editor.isActive(name, attrs);
+
+	$: menuItems = [
+		{
+			name: 'heading-2',
+			command: toggleHeading(2),
+			content: 'H2',
+			active: () => isActive('heading', { level: 2 }),
+		},
+		{
+			name: 'heading-3',
+			command: toggleHeading(3),
+			content: 'H3',
+			active: () => isActive('heading', { level: 3 }),
+		},
+		{
+			name: 'heading-4',
+			command: toggleHeading(4),
+			content: 'H4',
+			active: () => isActive('heading', { level: 4 }),
+		},
+		{
+			name: 'bold',
+			command: toggleBold,
+			content: 'B',
+			active: () => isActive('bold'),
+		},
+		{
+			name: 'italic',
+			command: toggleItalic,
+			content: 'I',
+			active: () => isActive('italic'),
+		},
+		{
+			name: 'strike',
+			command: toggleStrike,
+			content: 'S',
+			active: () => isActive('strike'),
+		},
+		{
+			name: 'inline-code',
+			command: toggleInlineCode,
+			content: 'Code',
+			active: () => isActive('code'),
+		},
+		{
+			name: 'highlight',
+			command: toggleHighlight,
+			content: 'H',
+			active: () => isActive('highlight'),
+		},
+		{
+			name: 'link',
+			command: setLink,
+			content: 'Link',
+			active: () => isActive('link'),
+		},
+		{
+			name: 'unlink',
+			command: () => $editor.chain().focus().unsetLink().run(),
+			content: 'Unlink',
+			active: () => isActive('unlink'),
+		},
+		{
+			name: 'paragraph',
+			command: setParagraph,
+			content: 'P',
+			active: () => isActive('paragraph'),
+		},
+		{
+			name: 'bullet-list',
+			command: toggleBulletList,
+			content: 'UL',
+			active: () => isActive('bulletList'),
+		},
+		{
+			name: 'ordered-list',
+			command: toggleOrderedList,
+			content: 'OL',
+			active: () => isActive('orderedList'),
+		},
+		{
+			name: 'code-block',
+			command: toggleCodeBlock,
+			content: '```',
+			active: () => isActive('codeBlock'),
+		},
+		{
+			name: 'hard-break',
+			command: setHardBreak,
+			content: '<br>',
+			active: () => isActive('hardBreak'),
+		},
+		{
+			name: 'divider',
+			command: setDivider,
+			content: '---',
+			active: () => isActive('horizontalRule'),
+		},
+	];
 </script>
 
-<div class = "border rounded-lg p-4">
-	<FixedMenu {editor} />
-	<div
-		bind:this = {element}
-		class = "py-4"
-	/>
-</div>
+<svelte:head>
+	<title>Tiptap Svelte</title>
+</svelte:head>
+
+<h1 class="mb-2 font-bold">Editor with Nodeview Renderer</h1>
 
 {#if editor}
-	<div class = "border mt-8">
-		<pre class = "mb-4 border-b">{JSON.stringify(editor.getJSON(), null, 2)}</pre>
-
-		<pre class = "font-mono bg-gray-200 mb-4">{editor.getHTML()}</pre>
-
-		<div class = "bg-indigo-100">{$contentStore}</div>
+	<div class="border-black border-2 border-b-0 rounded-t-md p-2 flex gap-1">
+		{#each menuItems as item (item.name)}
+			<button
+				type="button"
+				class="hover:bg-black hover:text-white w-auto h-7 px-1 rounded {item.active()
+				? 'bg-black text-white' : ''}"
+				on:click={item.command}
+			>
+				{item.content}
+			</button>
+		{/each}
 	</div>
 {/if}
 
-<style>
-  button.active {
-    background: black;
-    color: white;
-  }
+<EditorContent editor={$editor} />
 
-	.tiptap h1 {
-		font-size: 2em;
-		font-weight: bold;
-	}
-</style>
+{#if editor}
+	<pre class="json-output">{JSON.stringify($editor.getJSON(), null, 2)}</pre>
+{/if}
