5ebaf2d7755b44a35c8afb11f647ce64606502ca
李大毛 <github@darmau.co>
2024-06-27 18:46:13 +0900

    简化请求方式

--- src/routes/admin/article/new/+page.server.ts
+++ src/routes/admin/article/new/+page.server.ts
@@ -1,19 +1,10 @@
 import type { PageServerLoad } from './$types';
-import { ThirdPartyAPIs } from '$lib/types/thirdPartyApi';
 
-const storageConfigs = new ThirdPartyAPIs();
-const CONFIGS = storageConfigs.emptyObject();
-
-export const load: PageServerLoad = async ({ fetch, locals: { supabase }, url}) => {
+export const load: PageServerLoad = async ({ fetch, locals: { supabase }}) => {
 	const storageKeys = await fetch('/api/kv', {
 		method: 'POST',
 		body: JSON.stringify({ keys: ['config_URL_PREFIX']})
 	}).then(res => res.json());
-
-	storageKeys.forEach((item: Item) => {
-		const key = Object.keys(item)[0];
-		CONFIGS[key] = item[key];
-	});
 
 	// 从language表中获取is_default为true的语言
 	const defaultLanguage = await supabase
@@ -23,11 +14,7 @@ export const load: PageServerLoad = async ({ fetch, locals: { supabase }, url}) 
 		.single();
 
 	return {
-		prefix: CONFIGS.config_URL_PREFIX,
+		prefix: storageKeys[0].config_URL_PREFIX,
 		defaultLanguage: defaultLanguage.data
 	}
-}
-
-interface Item {
-	[key: string]: string;
 }
