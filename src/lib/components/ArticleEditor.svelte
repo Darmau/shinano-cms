60efa4434a03ec7054e28a5e8981bc52483c5f02
李大毛 <github@darmau.co>
2024-06-27 20:03:12 +0900

    fix: bind values for category

--- src/lib/components/ArticleEditor.svelte
+++ src/lib/components/ArticleEditor.svelte
@@ -154,6 +154,7 @@
 				</button>
 			</header>
 			<select
+				bind:value={articleContent.category}
 				id="category"
 				name="category"
 				class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
