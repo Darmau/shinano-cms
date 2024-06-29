096e72bfb5236234601d4b654d9d2caca4af9cc8
李大毛 <github@darmau.co>
2024-06-29 23:54:46 +0900

    style: 修改编辑器界面

--- src/lib/components/ArticleEditor.svelte
+++ src/lib/components/ArticleEditor.svelte
@@ -189,7 +189,7 @@
 					bind:value = {articleContent.title}
 					required
 					class =
-						"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-900 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+						"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
 					placeholder = "必须填写标题"
 				>
 			</div>
@@ -207,7 +207,7 @@
 					bind:value = {articleContent.slug}
 					required
 					class =
-						"block font-mono w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-900 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+						"block font-mono w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
 				>
 			</div>
 		</div>
@@ -224,7 +224,7 @@
 					bind:value = {articleContent.subtitle}
 					required
 					class =
-						"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-900 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+						"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
 				>
 			</div>
 		</div>
@@ -359,7 +359,7 @@
 					bind:value = {articleContent.abstract}
 					placeholder = "使用AI为文章生成摘要"
 					class =
-						"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-900 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+						"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
 				/>
 			</div>
 		</div>
