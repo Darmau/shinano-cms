1715feb18e2fe7544f77d1846406a1a671ecd0e8
李大毛 <github@darmau.co>
2024-07-05 20:54:52 +0900

    perf: 去除重复的class定义

--- src/lib/components/image/ImageGrid.svelte
+++ src/lib/components/image/ImageGrid.svelte
@@ -246,9 +246,3 @@
 		</div>
 	{/each}
 </div>
-
-<style>
-  .img-bg {
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.1' width='200' height='400'%3E%3Cdefs%3E%3Cpattern id='grid' width='20' height='20' patternUnits='userSpaceOnUse'%3E%3Crect fill='black' x='0' y='0' width='10' height='10' opacity='0.05'/%3E%3Crect fill='white' x='10' y='0' width='10' height='10'/%3E%3Crect fill='black' x='10' y='10' width='10' height='10' opacity='0.05'/%3E%3Crect fill='white' x='0' y='10' width='10' height='10'/%3E%3C/pattern%3E%3C/defs%3E%3Crect fill='url(%23grid)' x='0' y='0' width='100%25' height='100%25'/%3E%3C/svg%3E");
-  }
-</style>
